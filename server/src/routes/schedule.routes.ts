68e36a8306b6027edbfe3acee1e52a463fe78ce6
louis-prevosteau <louis.prevosteau@laposte.net>
2023-01-04 10:25:19 +0100

    fix: getSchedules (api)

--- server/src/routes/schedule.routes.ts
+++ server/src/routes/schedule.routes.ts
@@ -6,7 +6,7 @@ import { ADMIN } from "../utils/roles";
 const router = Router();
 const controller = new ScheduleController();
 
-router.get('/', jwtAuth, checkRole([ADMIN]), controller.getSchedules);
+router.get('/', controller.getSchedules);
 router.post('/', jwtAuth, checkRole([ADMIN]), controller.createSchedule);
 router.put('/:id', jwtAuth, checkRole([ADMIN]), controller.updateSchedule);
 router.delete('/:id', jwtAuth, checkRole([ADMIN]), controller.deleteSchedule);
